402648b91a8e97ff09d5d20904c3dac33e141812
vvilliam <wpalin@gmail.com>
2020-02-24 20:04:52 -0500

    refactor(courts_db): Remove old API call

--- courts_db/__init__.py
+++ courts_db/__init__.py
@@ -29,89 +29,6 @@ except:
     pass
 
 
-def find_court(court_str, filed_date=None, bankruptcy=False):
-    """
-    :param court_str:
-    :param filed_date:
-    :param regexes:
-    :return:
-    """
-    cd = {}
-    cdd = []
-    court_matches = []
-
-    assert (
-        type(court_str) == six.text_type
-    ), "court_str is not a text type, it's of type %s" % type(court_str)
-    for regex, court_id, court_name, court_type in regexes:
-        match = re.search(regex, court_str)
-        if match:
-            court_matches.append(court_id)
-            if court_id is None:
-                court_id = court_name
-            cd[match.group()] = court_id
-            cdd.append(
-                {
-                    "id": court_id,
-                    "text": match.group(),
-                    "court_name": court_name,
-                    "court_type": court_type,
-                }
-            )
-            # print(cdd)
-
-    results = list(set(court_matches))
-    if len(results) > 1:
-        court_matches = []
-        remove_list = [x["text"] for x in cdd]
-        subsetlist = []
-
-        for test in remove_list:
-            for item in [x for x in remove_list if x != test]:
-                if test in item:
-                    subsetlist.append(test)
-        final_list = [x for x in remove_list if x not in subsetlist]
-
-        for r in cdd:
-            if r["text"] in final_list:
-                court_key = r["id"]
-                court_matches.append(court_key)
-
-    court_matches = list(set(court_matches))
-
-    if len(court_matches) > 1:
-        new_cd = [x for x in cdd if x["id"] in court_matches]
-        bank = list(
-            set([x["id"] for x in new_cd if x["court_type"] == "bankruptcy"])
-        )
-        non_bank = list(
-            set([x["id"] for x in new_cd if x["court_type"] != "bankruptcy"])
-        )
-
-        if bankruptcy == True:
-            if len(bank) == 1:
-                return bank
-            else:
-                if len(non_bank) == 1:
-                    return non_bank
-        else:
-            return non_bank
-
-    return court_matches
-
-
-def find_court_info(court_str, filed_date=None, bankruptcy=False):
-    """
-
-    :param court_str:
-    :param filed_date:
-    :param bankruptcy:
-    :return:
-    """
-    matches = find_court(court_str, filed_date, bankruptcy)
-    return [court_dict[x] for x in matches]
-
-
 def find_court_ids_by_name(court_str):
 
     if not type(court_str) == six.text_type:
@@ -130,7 +47,7 @@ def find_court_ids_by_name(court_str):
     return list(court_matches)
 
 
-def filter_courts_by_date(found_ids, date_found, strict=False):
+def filter_courts_by_date(matches, date_found, strict=False):
     """
 
     :param found_ids:
@@ -139,7 +56,7 @@ def filter_courts_by_date(found_ids, date_found, strict=False):
     :return:
     """
     date_format = "%Y-%m-%d"
-    results = [court for court in courts if court["id"] in found_ids]
+    results = [court for court in courts if court["id"] in matches]
     filtered_results = []
     for result in results:
         for date_object in result["dates"]:
@@ -169,8 +86,8 @@ def filter_courts_by_date(found_ids, date_found, strict=False):
     return filtered_results
 
 
-def filter_courts_by_bankruptcy(found_ids, bankruptcy):
-    results = [court for court in courts if court["id"] in found_ids]
+def filter_courts_by_bankruptcy(matches, bankruptcy):
+    results = [court for court in courts if court["id"] in matches]
     if bankruptcy:
         return [
             court["id"] for court in results if court["type"] == "bankruptcy"
@@ -178,14 +95,39 @@ def filter_courts_by_bankruptcy(found_ids, bankruptcy):
     return [court["id"] for court in results if court["type"] != "bankruptcy"]
 
 
-def filter_courts_by_state(found_ids, state):
+def filter_courts_by_state(matches, state):
     state = find_state(state)
     return [
         court["id"]
         for court in courts
-        if court["id"] in found_ids and court["location"] == state
+        if court["id"] in matches and court["location"] == state
     ]
 
 
 def find_court_by_id(court_id):
     return [court for court in courts if court["id"] == court_id]
+
+
+def find_court(
+    court_str, bankruptcy=False, state=None, date_found=None, strict=False
+):
+    """
+
+    :param court_str:
+    :param bankruptcy:
+    :param state:
+    :param date_found:
+    :param strict:
+    :return:
+    """
+
+    matches = find_court_ids_by_name(court_str)
+    matches = filter_courts_by_bankruptcy(matches=matches, bankruptcy=bankruptcy)
+    if state:
+        matches = filter_courts_by_state(matches=matches, state=state)
+    if date_found:
+        matches = filter_courts_by_date(
+            matches=matches, date_found=date_found, strict=strict
+        )
+
+    return matches
