c8a5e3d61cc1db958dfe5c87c3f1c16354040d7f
William Palin <bill@free.law>
2022-12-02 12:37:21 -0500

    Version bump v0.10.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION = "0.10.11"
+VERSION = "0.10.12"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
